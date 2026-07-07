0bf00880fa645def4184354c98333ecb2e25b477
izzy <leonardogoncalvesdasilva1234@gmail.com>
2026-07-07 18:00:25 -0300

    Corrige integração Supabase

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://ywpjippxynugqgwhlvsw.supabase.co";
-const supabaseKey = "SUA_CHAVE_PUBLICAVEL";
+const supabaseKey = "sb_publishable_-33tP63hIJ6kj9n0Jtw6jg_caEqBpXq";
 
 
 const clienteDB = window.supabase.createClient(
