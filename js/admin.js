13e0932ff2974ed104056d355e9be041fc755fa1
izzy <leonardogoncalvesdasilva1234@gmail.com>
2026-07-12 12:26:01 -0300

    novo test painel admin

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://ywpjippxynugqgwhlvsw.supabase.co";
-const supabaseKey = "SUA_CHAVE_PUBLICA";
+const supabaseKey = "sb_publishable_-33tP63hIJ6kj9n0Jtw6jg_caEqBpXq";
 
 const db = window.supabase.createClient(
     supabaseUrl,
